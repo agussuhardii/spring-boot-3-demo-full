e5339c82561558e94b5f8dd6c1a8592e4f23b8ec
agussuhardii <agus.suhardii@gmail.com>
2023-06-15 07:02:10 +0700

    add validation and global response

--- src/main/java/com/agussuhardi/springdemofull/config/security/SecurityConfig.java
+++ src/main/java/com/agussuhardi/springdemofull/config/security/SecurityConfig.java
@@ -2,13 +2,10 @@ package com.agussuhardi.springdemofull.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * @author agus.suhardii@gmail.com
@@ -19,7 +16,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,8 +29,11 @@ public class SecurityConfig {
     }
 
 
-
-//    ignore auth request
+    /**
+     * ignore rest api url and register by bean
+     *
+     * @return ignore url security config
+     */
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers("/api/v1/auth/register", "/ignore2");
