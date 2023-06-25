7f89237ab972c33ecc49905c502357c802eea7c9
agussuhardii <agus.suhardii@gmail.com>
2023-06-25 19:01:12 +0700

    init product scheme

---
+++ src/main/resources/db/changelog/product/ddl/20230625_init product.sql
@@ -0,0 +1,48 @@
+create table category
+(
+    id          char(36)     not null,
+    name        varchar(100) not null,
+    text        text,
+    icon        varchar(255),
+
+    created_by  varchar(50),
+    modified_by varchar(50),
+    created_at  numeric,
+    updated_at  numeric,
+    is_deleted  boolean      not null default false,
+    primary key (id)
+);
+
+
+create table product
+(
+    id          char(36)     not null,
+    name        varchar(100) not null,
+    text        text,
+    image       varchar(255),
+
+    created_by  varchar(50),
+    modified_by varchar(50),
+    created_at  numeric,
+    updated_at  numeric,
+    is_deleted  boolean      not null default false,
+    primary key (id)
+);
+
+
+create table product_qty
+(
+    id          char(36) not null,
+    product_id  char(36) not null,
+    qty         numeric  not null,
+    adjust      numeric  not null,
+
+    created_by  varchar(50),
+    modified_by varchar(50),
+    created_at  numeric,
+    updated_at  numeric,
+    is_deleted  boolean  not null default false,
+    primary key (id)
+);
+
+
